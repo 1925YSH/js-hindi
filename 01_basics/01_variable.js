5ee55f4d90b03371eef51941e63f652162fe6812
YASH OTWAL <128904224+1925YSH@users.noreply.github.com>
2024-06-27 13:14:04 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId =12345
+let accountEmail ="acc@gmail.com"
+var accPassword ="onetothree"
+city="jaipur"
+
+let name;
+accountEmail="new@gmail.com"
+console.log(accountId);
+console.table([accountId,accountEmail,accPassword,city,name])
+
+/*
+not to use var because of issue of block scope and function scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+console.log("YASH")
+
+let name="YAsh Otwal"
+let age=18
+let isLogggedIn=false
+
+console.table([name,age,isLogggedIn])
+
+console.log(typeof "Yash Otwal") // typtof variable ka type bta deta hai
