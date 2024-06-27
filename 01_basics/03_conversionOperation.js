38e7f7215a148b3a3df43ead62c8dd10fe5d1894
YASH OTWAL <128904224+1925YSH@users.noreply.github.com>
2024-06-27 14:05:57 +0000

    container config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);//multiply
+// console.log(2**2);//square of 2
+// console.log(2**3);//cube of 2
+// console.log(2/2);
+// console.log(2%3);
+
+let str1="Yash"
+let str2=" Otwal"
+let str3=str1+str2 //ese 2 string bhi add ki ja skti hai
+
+console.log(str3);
+
+
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,1 @@
+console.log("2">1);//automatic string ko number maan liya js ne or compare krr k bta diya
