b19c545e373a02d9cbd9b3c0f8b7b94e0028d6a2
YASH OTWAL <128904224+1925YSH@users.noreply.github.com>
2024-06-29 14:06:00 +0000

    container config done

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -8,6 +8,12 @@ const myArry1=[10,12,123,145,"it can hold mixture of types"]
 console.log(myArry1);
 
 const myArr2 =new Array(1,2,3,4)
-myArr2.push(5)//push to add
-myArr2.pop()//pop removes last value of the array
+// myArr2.push(5)//push to add
+// myArr2.pop()//pop removes last value of the array
+// console.log(myArr2.includes(2));
+
+const newArray =myArr2.join()// join() method convert the array into string
+
 console.log(myArr2);
+console.log(newArray);
+console.log(typeof newArray);
