715f97eb526eedbac260b537d462f2d8371a169e
YASH OTWAL <128904224+1925YSH@users.noreply.github.com>
2024-06-29 11:37:34 +0000

    container config done

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,5 @@
+let myDate =new Date()//object of date
+console.log(myDate.toString());//this method gives today current date
+
+console.log(myDate.toDateString()); //gives date
+console.log(myDate.toLocaleString());
