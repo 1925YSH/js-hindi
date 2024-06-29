43603e6a794895fa9c9684cbf2ff77371a85be05
YASH OTWAL <128904224+1925YSH@users.noreply.github.com>
2024-06-29 13:51:40 +0000

    container config done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,13 @@
+//ways to declear arrays in js
+const myArry=[10,12,123,145]
+
+console.log(myArry);
+
+const myArry1=[10,12,123,145,"it can hold mixture of types"]
+
+console.log(myArry1);
+
+const myArr2 =new Array(1,2,3,4)
+myArr2.push(5)//push to add
+myArr2.pop()//pop removes last value of the array
+console.log(myArr2);
