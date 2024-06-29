a6a3d3f49aa84f86f645c90c44c608405efc1343
YASH OTWAL <128904224+1925YSH@users.noreply.github.com>
2024-06-29 14:21:24 +0000

    container config done

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,13 @@
+const arr1=[1,2,3,4]
+
+const arr2=[5,6,7,8]
+
+console.log(arr1);
+console.log(arr2);
+
+const arr3=arr1.concat(arr2) //to concate array
+
+console.log(arr3);
+
+const arr4=[...arr1,...arr2]//it is spread method to concate array by using ... this three dots
+console.log(arr4)
