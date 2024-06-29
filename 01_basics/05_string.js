ea1e9bf5e956746ad43bca77330dd2417d534dfb
YASH OTWAL <128904224+1925YSH@users.noreply.github.com>
2024-06-29 10:58:22 +0000

    conainer confi done

---
+++ 01_basics/05_string.js
@@ -0,0 +1,8 @@
+const gameName= new String("fkdfdkkjn")//way to define string like object in js issmr . lga k string k function use krr skte hai jese neeche kiya hai
+
+console.log(gameName);
+console.log(gameName.toUpperCase());
+
+let name="flsdfl"
+console.log(name.toUpperCase());// ye . lga k direct methods use krr skte hai string ki
+console.log(gameName.charAt(2));
---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,10 @@
+const score =400
+
+const balance = new Number(400)//varialbe ko number declear krrne k tarika hai 
+
+console.log(score);
+console.log(balance);
+console.log(balance.toString().toLocaleLowerCase());//upper object bnn gya hai toh ese operations bhi lga skte hai
+
+const other= 1000000
+console.log(other.toLocaleString('en-IN'));//khud se comma include krr dega smjhane kliye in indian form
