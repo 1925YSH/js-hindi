e8d515d50096fb6deca5cbccce04fd142f407f19
YASH OTWAL <128904224+1925YSH@users.noreply.github.com>
2024-07-06 06:59:03 +0000

    container config done

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,12 @@
+function one() {
+
+    const username="Hitesh"
+    
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+  //  console.log(website);bhaar vala andar vale ko access nhi krr skta 
+    two()
+}
+one()
