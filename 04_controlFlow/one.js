ff60f8c1fe84e28bb84ffa414f44ed82a8181a09
YASH OTWAL <128904224+1925YSH@users.noreply.github.com>
2024-07-08 06:26:39 +0000

    container config done

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,74 @@
+//if or else k baare m btaya
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
+//---------------------------------------------------
+
+const month = 4
+switch (month) {
+    case 1:
+            console.log("Jan");
+        break;
+    case 2:
+            console.log("Feb");
+        break;
+    case 3:
+            console.log("March");
+        break;
+    case 4:
+            console.log("April");
+        break;
+
+    default:
+        console.log("none");
+        break;
+}
